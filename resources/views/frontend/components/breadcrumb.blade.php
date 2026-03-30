33fee5bcea7c8e01212397a24d4ec51f07c8ecf2
Md Razu Hossain Raj <mdrazuhossainraj@gmail.com>
2026-03-30 17:47:31 +0600

    update

---
+++ resources/views/frontend/components/breadcrumb.blade.php
@@ -0,0 +1,14 @@
+
+<section class="breadcrumb-section" style="background-image: url('{{ asset('contents/website/assets/images/banner/breadcrumb.jpg') }}');">
+  <div class="breadcrumb-overlay"></div>
+  <div class="container text-center">
+    <h1 class="page-title">Current Page Title</h1>
+    <nav aria-label="breadcrumb">
+      <ol class="breadcrumb modern-breadcrumb">
+        <li class="breadcrumb-item"><a href="#">Home</a></li>
+        <li class="breadcrumb-item"><a href="#">Category</a></li>
+        <li class="breadcrumb-item active" aria-current="page">Current Page</li>
+      </ol>
+    </nav>
+  </div>
+</section>
