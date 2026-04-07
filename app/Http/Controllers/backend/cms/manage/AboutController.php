28d78c14a1e5572e84b855ca3e55ee13840f8d3e
Md Razu Hossain Raj <84346306+razu2021@users.noreply.github.com>
2026-04-07 22:04:53 +0600

    update

---
+++ app/Http/Controllers/backend/cms/manage/AboutController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\backend\cms\manage;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class AboutController extends Controller
+{
+    //
+}
