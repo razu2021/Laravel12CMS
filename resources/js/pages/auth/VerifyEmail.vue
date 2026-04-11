93997591f8bf4c71b59d431484dac6606fc4b337
Md Razu Hossain Raj <84346306+razu2021@users.noreply.github.com>
2026-04-12 00:48:09 +0600

    update

--- resources/js/pages/auth/VerifyEmail.vue
+++ resources/js/pages/auth/VerifyEmail.vue
@@ -3,7 +3,7 @@ import TextLink from '@/components/TextLink.vue';
 import { Button } from '@/components/ui/button';
 import { Spinner } from '@/components/ui/spinner';
 import AuthLayout from '@/layouts/AuthLayout.vue';
-import { logout } from '@/routes';
+import { logout } from '@/routes/index';
 import { send } from '@/routes/verification/index';
 import { Form, Head } from '@inertiajs/vue3';
 
