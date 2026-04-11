930a41879c0d75f1ebea2bcb18dd7103d7019db3
Md Razu Hossain Raj <84346306+razu2021@users.noreply.github.com>
2026-04-12 00:31:41 +0600

    update

--- resources/js/pages/settings/Appearance.vue
+++ resources/js/pages/settings/Appearance.vue
@@ -7,7 +7,7 @@ import { type BreadcrumbItem } from '@/types';
 
 import AppLayout from '@/layouts/AppLayout.vue';
 import SettingsLayout from '@/layouts/settings/Layout.vue';
-import { edit } from '@/routes/appearance';
+import { edit } from '@/routes/appearance/index';
 
 const breadcrumbItems: BreadcrumbItem[] = [
     {
