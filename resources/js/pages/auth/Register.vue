60ca8261db538fd681bd24be711f8635773b643a
Md Razu Hossain Raj <84346306+razu2021@users.noreply.github.com>
2026-04-12 12:27:29 +0600

    update

--- resources/js/pages/auth/Register.vue
+++ resources/js/pages/auth/Register.vue
@@ -7,6 +7,7 @@ import { Label } from '@/components/ui/label';
 import { Spinner } from '@/components/ui/spinner';
 import AuthBase from '@/layouts/AuthLayout.vue';
 import { login } from '@/routes/index';
+
 import { store } from '@/routes/register/index';
 import { Form, Head } from '@inertiajs/vue3';
 </script>
